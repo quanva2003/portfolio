5367aeff79882653fdcbca1adb750c3fd32da057
quanva2003 <qsao2212@gmail.com>
2026-04-22 22:22:02 +0700

    fix: remove dead project link buttons (no live URLs available)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,12 +32,6 @@ const Projects = () => {
               <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-[#00fbff] to-black/10 flex justify-center items-center flex-col p-16 text-center translate-y-full group-hover:translate-y-0 transition duration-500">
                 <h4 className="text-2xl font-semibold">{project.title}</h4>
                 <p className="text-base my-1 mb-4">{project.description}</p>
-                <a
-                  href="#"
-                  className="inline-flex justify-center items-center w-14 h-14 bg-[#ffffffe2] rounded-full"
-                >
-                  <i className="bx bx-link-external text-xl text-[#323946]"></i>
-                </a>
               </div>
             </div>
             {/* Title hiển thị dưới image trên mobile */}
